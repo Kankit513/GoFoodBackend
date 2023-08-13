e08414c2825b04ac16f68cd3a457bab7b2433aae
Kankit513 <ankitkumar9155437613@gmail.com>
2023-08-13 13:45:43 +0530

    Check

--- index.js
+++ index.js
@@ -8,14 +8,14 @@ const port = process.env.PORT;
 const mongoDB = require('./db');
 mongoDB();
 
-app.use((req,res,next)=>{
-    res.setHeader("Access-Control-Allow-Origin","http://localhost:3000");
-    res.header(
-        "Access-Control-Allow-Headers",
-        "Origin,X-Requested-With,Content-Type,Accept"
-    );
-    next();
-})
+// app.use((req,res,next)=>{
+//     res.setHeader("Access-Control-Allow-Origin","http://localhost:3000");
+//     res.header(
+//         "Access-Control-Allow-Headers",
+//         "Origin,X-Requested-With,Content-Type,Accept"
+//     );
+//     next();
+// })
 
 app.use(express.json())
 app.use('/api',require('./Routes/CreatUser'));
